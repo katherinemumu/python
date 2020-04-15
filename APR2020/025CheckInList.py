7e3a6991e11ed7e78e3e3dcdcb362492ecef9ab7
Katherine Mu <katherinemu@192.168.0.110>
2020-04-14 23:14:46 -0400

    more stuff

---
+++ APR2020/025CheckInList.py
@@ -0,0 +1,13 @@
+'''
+katherinemumu
+Apr 14 2020
+This program will check if a given value is in a group of values
+'''
+
+l = [1, 3, 4]
+n = 1
+
+if (n in l):
+    print("Yes the value is in")
+else:
+    print("Nopeeee")
---
+++ APR2020/026Histogram.py
@@ -0,0 +1,12 @@
+'''
+katherinemumu
+Apr 14 2020
+This program will create a histogram
+'''
+
+l = [2, 3, 4, 5]
+
+for i in l:
+    for j in range(i):
+        print("*", end = "")
+    print()
